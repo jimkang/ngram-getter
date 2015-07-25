dd7048051e4e9cef73b21b8acc4705443ad7aff2
Jim Kang <jimkang@gmail.com>
2015-07-24 23:15:01 -0400

    Added html parser and test.

---
+++ parse-ngram-html.js
@@ -0,0 +1,42 @@
+var cheerio = require('cheerio');
+var bracketsRegex = /\[(.*?)\]/g;
+
+function parseNgramHTML(html) {
+  var $ = cheerio.load(html);
+
+  var content = $('meta[itemprop=description]').attr('content');
+  
+  return content.match(bracketsRegex)
+    .map(chopOffEnds)
+    .map(ngramsFromNgramsString);
+}
+
+function chopOffEnds(s) {
+  if (s.length > 1) {
+    return s.slice(1, s.length - 2);
+  }
+  else {
+    return s;
+  }
+}
+
+function ngramsFromNgramsString(ngramsString) {
+  return ngramsString.split(' ').map(ngramFromString);
+}
+
+function ngramFromString(piece) {
+  if (piece.indexOf('_') === -1) {
+    return {
+      word: piece
+    };
+  }
+  else {
+    var wordPOSPair = piece.split('_');
+    return {
+      word: wordPOSPair[0],
+      pos: wordPOSPair[1].toLowerCase()
+    };
+  }
+}
+
+module.exports = parseNgramHTML;
--- tests/parse-tests.js
+++ tests/parse-tests.js
@@ -1,11 +1,138 @@
 var test = require('tape');
 var parseNgramHTML = require('../parse-ngram-html');
+var fs = require('fs');
+
+var ngramHTML = fs.readFileSync(__dirname + '/data/ngram.html');
 
 test('Parse test', function parseTest(t) {
   t.plan(1);
 
-  var opts = {
+  var ngrams = parseNgramHTML(ngramHTML);
 
-  };
-
+  t.deepEqual(
+    ngrams,
+    [
+      [
+        {
+          word: 'call',
+          pos: 'verb'
+        },
+        {
+          word: 'the'
+        },
+        {
+          word: 'cops'
+        }
+      ],
+      [
+        {
+          word: 'called',
+          pos: 'verb'
+        },
+        {
+          word: 'the'
+        },
+        {
+          word: 'cops'
+        }
+      ],
+      [
+        {
+          word: 'tell',
+          pos: 'verb'
+        },
+        {
+          word: 'the'
+        },
+        {
+          word: 'cops'
+        }
+      ],
+      [
+        {
+          word: 'told',
+          pos: 'verb'
+        },
+        {
+          word: 'the'
+        },
+        {
+          word: 'cops'
+        }
+      ],
+      [
+        {
+          word: 'calling',
+          pos: 'verb'
+        },
+        {
+          word: 'the'
+        },
+        {
+          word: 'cops'
+        }
+      ],
+      [
+        {
+          word: 'get',
+          pos: 'verb'
+        },
+        {
+          word: 'the'
+        },
+        {
+          word: 'cops'
+        }
+      ],
+      [
+        {
+          word: 'Call',
+          pos: 'verb'
+        },
+        {
+          word: 'the'
+        },
+        {
+          word: 'cops'
+        }
+      ],
+      [
+        {
+          word: 'have',
+          pos: 'verb'
+        },
+        {
+          word: 'the'
+        },
+        {
+          word: 'cops'
+        }
+      ],
+      [
+        {
+          word: 'let',
+          pos: 'verb'
+        },
+        {
+          word: 'the'
+        },
+        {
+          word: 'cops'
+        }
+      ],
+      [
+        {
+          word: 'are',
+          pos: 'verb'
+        },
+        {
+          word: 'the'
+        },
+        {
+          word: 'cops'
+        }
+      ]
+    ],
+    'The correct ngrams are returned.'
+  );
 });
